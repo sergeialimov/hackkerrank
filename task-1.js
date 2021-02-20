541f4a20b6a20501279b2d5ea9df8f27477d9d0c
sergeialimov <8912652@gmail.com>
2021-02-20 16:15:25 +0300

    add tmp variables

--- task-1.js
+++ task-1.js
@@ -1,17 +1,21 @@
 const { log } = require('console');
 const fs = require('fs');
 
+const inNum = (string) => Number.isInteger(+string);
 
-function decryptPassword(str) {
+
+function decryptPassword(pass) {
   let newPass = '';
-  for (let i = 0; i < str.length; i++) {
-      if (i < str.length - 1) {
-        if (str[i] === str[i].toLowerCase() && str[i + 1] === str[i + 1].toUpperCase()) {
-          newPass = newPass.concat(str[i + 1], str[i], '*');
+  for (let i = 0; i < pass.length; i++) {
+      let strI = pass[i];
+      let strI1 = pass[i+1];
+      if (i < pass.length - 1) {
+        if (pass[i] === pass[i].toLowerCase() && pass[i + 1] === pass[i + 1].toUpperCase()) {
+          newPass = newPass.concat(pass[i + 1], pass[i], '*');
         }
         
-        if (Number.isInteger(+str[i])) {
-          console.log('-- -- -- s[i]', str[i]);
+        if (inNum(pass[i])) {
+          console.log('-- -- -- s[i]', pass[i]);
           
           newPass = newPass.concat('0');
         }
@@ -23,11 +27,11 @@ function decryptPassword(str) {
 function main() {
   // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
   
-  const s = "hAc";
+  const incomingPass = "hAc";
   // const s = "hAck3rr4nk";
-  console.log('start', s);
+  console.log('start', incomingPass);
 
-  const result = decryptPassword(s);
+  const result = decryptPassword(incomingPass);
   console.log('result', result);
 
   // ws.write(result + '\n');
