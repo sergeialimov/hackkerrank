6c3e2fcd85290b5f45b1e0db54fd615db034d6c7
sergeialimov <8912652@gmail.com>
2021-02-20 16:43:57 +0300

    rework if-else structure

--- task-1.js
+++ task-1.js
@@ -2,18 +2,20 @@ const inNum = (string) => Number.isInteger(+string);
 
 
 const decryptPassword = (pass) => {
-  const expectedResult = '43Ah*ck0rr0nk';
+  const expectedResult = '43Ah*c  k0rr0nk';
   let newPass = '';
 
   for (let i = 0; i < pass.length; i++) {
     let strI = pass[i];
     let strI1 = pass[i+1];
     if (i < pass.length - 1) {
-      if (pass[i] === pass[i].toLowerCase() && pass[i + 1] === pass[i + 1].toUpperCase()) {
-        newPass = newPass.concat(pass[i + 1], pass[i], '*');
-      }
       if (inNum(pass[i])) {
         newPass = newPass.concat('0');
+      } else if (pass[i] === pass[i].toLowerCase() && pass[i + 1] === pass[i + 1].toUpperCase()) {
+        newPass = newPass.concat(pass[i + 1], pass[i], '*');
+        i++;
+      } else {
+        newPass = newPass.concat(pass[i]);
       }
     }
   }
@@ -21,7 +23,7 @@ const decryptPassword = (pass) => {
 }
 
 const main = () => {
-  const incomingPass = "hAc"; // "hAck3rr4nk"
+  const incomingPass = "hAck"; // "hAck3rr4nk"
   const result = decryptPassword(incomingPass);
   console.log('-- result', result);
 }
