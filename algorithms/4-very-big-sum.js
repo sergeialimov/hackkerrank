1775860c0fd0df821996c1c92d45b185d33a8200
sergeialimov <8912652@gmail.com>
2021-02-23 12:59:38 +0300

    solution for 4-very-big-sum

--- algorithms/4-very-big-sum.js
+++ algorithms/4-very-big-sum.js
@@ -26,20 +26,30 @@ function readLine() {
 
 // Complete the aVeryBigSum function below.
 function aVeryBigSum(ar) {
-
-
+  console.log('-- -- -- ar', ar);
+  let res = 0;
+  ar.forEach(element => {
+      res+=element
+    });
+  console.log('-- -- -- res', res);
+  
+  return res;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  console.log('-- -- -- 1', );
+  const input = [ 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 ];
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const arCount = parseInt(readLine(), 10);
+    // const arCount = parseInt(readLine(), 10);
 
-    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+    // const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
 
-    let result = aVeryBigSum(ar);
+    let result = aVeryBigSum(input);
 
-    ws.write(result + "\n");
+    // ws.write(result + "\n");
 
-    ws.end();
+    // ws.end();
 }
+
+main();
