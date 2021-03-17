1bf20653c077104fd4f145d73c9cbf87a9918412
sergeialimov <8912652@gmail.com>
2021-03-17 16:09:17 +0300

    task-9 solution

--- algorithms/9-candles.js
+++ algorithms/9-candles.js
@@ -2,25 +2,6 @@
 
 const fs = require('fs');
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /*
  * Complete the 'birthdayCakeCandles' function below.
@@ -30,20 +11,19 @@ function readLine() {
  */
 
 function birthdayCakeCandles(candles) {
-    // Write your code here
-
+  if (!candles || candles.length === 0) {
+    return 0;
+  }
+  const tmp = [...candles];
+  const candle = tmp.sort().pop();
+  const candlesNum = tmp.filter((x) => x === candle);
+  return candlesNum.length + 1;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const candlesCount = parseInt(readLine().trim(), 10);
-
-    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
-
-    const result = birthdayCakeCandles(candles);
-
-    ws.write(result + '\n');
-
-    ws.end();
+  const candles = [1];
+  const result = birthdayCakeCandles(candles);
+  console.log('-- -- -- ', result);
 }
+
+main();
