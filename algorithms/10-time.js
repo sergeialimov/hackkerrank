2fe1fb5dd5f72a52c54eb86c1a74303755cf2f1d
sergeialimov <8912652@gmail.com>
2021-03-17 16:26:15 +0300

    task-10 add boilerplate

---
+++ algorithms/10-time.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('fs');
+
+
+/*
+ * Complete the timeConversion function below.
+ */
+function timeConversion(s) {
+  console.log('-- -- -- s', s);
+  
+}
+
+function main() {
+  const s = '07:05:45PM';
+  let result = timeConversion(s);
+
+}
+
+main();
