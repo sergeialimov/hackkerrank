ee42d4aaf6fb692a42880b74a7ef3b54f57e5970
sergeialimov <8912652@gmail.com>
2021-03-17 17:16:35 +0300

    task-10 solution

--- algorithms/10-time.js
+++ algorithms/10-time.js
@@ -6,14 +6,39 @@ const fs = require('fs');
 /*
  * Complete the timeConversion function below.
  */
+
+
 function timeConversion(s) {
-  console.log('-- -- -- s', s);
+  const daytime = s.slice(-2);
+
+  const tmp = s.slice(0, -2).split(':');
+
+  const makeHours = (str) => {
+    let hours = Number.parseInt(str);
+    if (daytime === 'PM') {
+      hours += 12;
+    }
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (hours === 24) {
+      hours = '12';
+    }
+    if (daytime === 'AM' && hours === 12) {
+      hours = `00`;
+    }
+    return hours;
+  }
+
+  return `${makeHours(tmp[0])}:${tmp[1]}:${tmp[2]}`;
   
 }
 
 function main() {
-  const s = '07:05:45PM';
+  // const s = '07:05:45AM';
+  const s = '12:01:45PM';
   let result = timeConversion(s);
+  console.log('-- -- -- ', result);
 
 }
 
